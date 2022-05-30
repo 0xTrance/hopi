91c6404578f0b058c44afacaba57ef6b83c551b6
Tamim Azmain <tamim.azmain@rooter.co.jp>
2022-05-31 01:20:00 +0900

    init

---
+++ spec/hopi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hopi do
+  it "has a version number" do
+    expect(Hopi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
